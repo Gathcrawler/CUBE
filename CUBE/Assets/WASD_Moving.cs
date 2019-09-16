4501e191d19d924dd1f1cf170cc3d6dbf0fcea87
DimaZykov <dimazykov@trilobitesoft.com>
2019-09-16 13:38:18 +0300

    Project

---
+++ CUBE/Assets/WASD_Moving.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Moving : MonoBehaviour 
+{
+
+	public float Speed = 10;
+	private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		if (moveHorizontal > 0)
+		{
+			transform.Translate(transform.right * Time.deltaTime * Speed);
+		}
+		else if (moveHorizontal < 0)
+		{
+			transform.Translate(-transform.right * Time.deltaTime * Speed);
+		}
+
+		if (moveVertical > 0)
+		{
+			transform.Translate(transform.forward * Time.deltaTime * Speed);
+		}
+		else if (moveVertical < 0)
+		{
+			transform.Translate(-transform.forward * Time.deltaTime * Speed);
+		}
+
+
+	}
+}
