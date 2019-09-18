df0b3f65636f96fd7cba395d8b0551df21670ab9
DimaZykov <dimazykov@trilobitesoft.com>
2019-09-18 13:47:31 +0300

    Include spawn cube clones in parent object

--- CUBE/Assets/Scripts/CUBE_Manager.cs
+++ CUBE/Assets/Scripts/CUBE_Manager.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CUBE_Manager : MonoBehaviour {
-
+public class CUBE_Manager : MonoBehaviour
+{
 	public GameObject prefab;
 	public int numberOfObjects = 5;
 	public float radius = 5f;
 	
-
+	
 
 	void Start() 
 	{
     	for (int i = 0; i < numberOfObjects; i++) 
 		{
         	float angle = i * Mathf.PI * 2 / numberOfObjects;
-        	Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle))* radius;
-        	Instantiate(prefab, pos, Quaternion.identity);
+        	Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+        	var child = Instantiate(prefab, pos, Quaternion.identity);
+			child.transform.SetParent(transform);
     	}
 	}
 }
