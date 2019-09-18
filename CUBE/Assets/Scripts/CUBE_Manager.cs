fdef583df4cc76b2efbc54498f2640d8c53c9569
DimaZykov <dimazykov@trilobitesoft.com>
2019-09-18 10:51:52 +0300

    r

--- CUBE/Assets/Scripts/CUBE_Manager.cs
+++ CUBE/Assets/Scripts/CUBE_Manager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class CUBE_Manager : MonoBehaviour {
 
 	public GameObject prefab;
-	public int numberOfObjects = 5;
-	public float radius = 5f;
+	public int numberOfObjects = 6;
+
 
 	void Start() 
 	{
     	for (int i = 0; i < numberOfObjects; i++) 
 		{
         	float angle = i * Mathf.PI * 2 / numberOfObjects;
-        	Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+        	Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
         	Instantiate(prefab, pos, Quaternion.identity);
     	}
 	}
