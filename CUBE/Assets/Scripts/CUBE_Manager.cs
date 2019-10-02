5ad382a7644a991aa07117fcc1179c2cc59f58fa
DimaZykov <dimazykov@trilobitesoft.com>
2019-10-02 11:30:10 +0300

    Create list on moving script

--- CUBE/Assets/Scripts/CUBE_Manager.cs
+++ CUBE/Assets/Scripts/CUBE_Manager.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class CUBE_Manager : MonoBehaviour
 {
 	public GameObject prefab;
+	public CubesMover script;
 	public int numberOfObjects = 5;
 	public float radius = 5f;
 	
+	public bool selectable = false;
+	public List<GameObject> cubes;
 	
-
 	void Start() 
 	{
+		cubes = new List<GameObject>();
     	for (int i = 0; i < numberOfObjects; i++) 
 		{
         	float angle = i * Mathf.PI * 2 / numberOfObjects;
         	Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
-        	var child = Instantiate(prefab, pos, Quaternion.identity);
+			var child = Instantiate(prefab, pos, Quaternion.identity);
 			child.transform.SetParent(transform);
+			cubes.Add(child);
     	}
+		CubesMover wasd = gameObject.GetComponent<CubesMover>();
+		wasd.Start();
 	}
+
+
+
+	
+	/*void Update ()
+    {
+     	if (Input.GetMouseButtonDown(0))
+     	{
+        	Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{
+				CubesMover wasd = gameObject.AddComponent<CubesMover>();
+			}
+     	}
+    } */   			
 }
---
+++ CUBE/Assets/Scripts/CubesMover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesMover : MonoBehaviour 
+{
+	public float Speed = 10;
+	public List<GameObject> MovingCubes = new List<GameObject>();
+	
+	public void Start()
+	{
+		foreach (Transform child in this.gameObject.transform)
+		{
+			MovingCubes.Add(child.gameObject);
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+			
+			if (moveHorizontal > 0)
+			{
+				transform.Translate(transform.right * Time.deltaTime * Speed);
+			}
+			else if (moveHorizontal < 0)
+			{
+				transform.Translate(-transform.right * Time.deltaTime * Speed);
+			}
+		
+
+			if (moveVertical > 0)
+			{
+				transform.Translate(transform.forward * Time.deltaTime * Speed);
+			}
+			else if (moveVertical < 0)
+			{
+				transform.Translate(-transform.forward * Time.deltaTime * Speed);
+			}
+
+	}
+}
